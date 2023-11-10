0d1d269e48cfc8720bf36ada7df208277a362fb3
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2023-11-10 01:47:54 +0100

    late update

--- chatGPT.py
+++ chatGPT.py
@@ -1,72 +1,103 @@
 import tkinter as tk
-import openai
+from tkinter import ttk, filedialog
+import requests
+import json
 
-# Set up API key
+API_TOKEN = "YOUR_HUGGING_FACE_API_TOKEN"
 
-openai.api_key = ""
+class HuggingFaceGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hugging Face Inference API GUI")
 
-# Create a function to make the API request and return the response
-def get_response(query):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=query,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
-    return response["choices"][0]["text"]
+        # Model options
+        self.model_options = [
+            "facebook/detr-resnet-50-panoptic",
+            "facebook/detr-resnet-50",
+            "google/vit-base-patch16-224",
+            "superb/hubert-large-superb-er",
+            "facebook/wav2vec2-base-960h"
+        ]
 
-# Create a Tkinter app
-# Create a Tkinter app
-app = tk.Tk()
-app.title("OpenAI Chat GPT")
+        # GUI components
+        self.create_widgets()
 
-app.geometry("432x195")
+    def create_widgets(self):
+        # Model selection
+        self.model_label = tk.Label(self.root, text="Select Model:")
+        self.model_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
 
-# Create a label for the token input
-token_label = tk.Label(app, text="API Key:")
-token_label.grid(row=0, column=0, padx=10, pady=10)
+        self.model_var = tk.StringVar()
+        self.model_var.set(self.model_options[0])
 
-# Create a text entry box for the API key
-token_text = tk.StringVar()
-token_box = tk.Entry(app, textvariable=token_text)
-token_box.grid(row=0, column=1, padx=10, pady=10)
+        self.model_dropdown = ttk.Combobox(self.root, textvariable=self.model_var, values=self.model_options)
+        self.model_dropdown.grid(row=0, column=1, padx=10, pady=5, columnspan=2)
 
-# Create a button to submit the token
-def submit_token():
-    openai.api_key = token_text.get()
-submit_token_button = tk.Button(app, text="Set Token", command=submit_token)
-submit_token_button.grid(row=0, column=2, padx=10, pady=10)
+        # Input section
+        self.input_label = tk.Label(self.root, text="Input:")
+        self.input_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
 
-# Create a text entry box for the user's query
-query_text = tk.StringVar()
-query_box = tk.Entry(app, textvariable=query_text)
-query_box.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+        self.input_text = tk.Text(self.root, height=6, width=50)
+        self.input_text.grid(row=1, column=1, padx=10, pady=5, columnspan=2)
 
-# Create a button to submit the query
-def submit_query():
-    query = query_text.get()
-    response = get_response(query)
-    response_label.config(text=response)
+        self.browse_button = tk.Button(self.root, text="Browse", command=self.browse_file)
+        self.browse_button.grid(row=2, column=0, padx=10, pady=5)
 
-submit_label = tk.Label(app, text="Submit")
-submit_label.grid(row=1, column=0)
+        # Output section
+        self.output_label = tk.Label(self.root, text="Output:")
+        self.output_label.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
 
-submit_button = tk.Button(app, text="Submit", command=submit_query)
-submit_button.grid(row=1, column=2)
+        self.output_text = tk.Text(self.root, height=6, width=50, state=tk.DISABLED)
+        self.output_text.grid(row=3, column=1, padx=10, pady=5, columnspan=2)
 
-# Create a label to display the response
-response_label = tk.Label(app, text="")
-response_label.grid()
+        # Send button
+        self.send_button = tk.Button(self.root, text="Send", command=self.send_request)
+        self.send_button.grid(row=4, column=0, columnspan=3, pady=10)
 
-# Create a button to clear the response label
-def clear_response():
-    response_label.config(text="")
-clear_button = tk.Button(app, text="Clear Answers", command=clear_response)
-clear_button.grid(column=1)
+    def browse_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            with open(file_path, "r") as file:
+                content = file.read()
+                self.input_text.delete(1.0, tk.END)
+                self.input_text.insert(tk.END, content)
+
+    def send_request(self):
+        selected_model = self.model_var.get()
+        input_text = self.input_text.get("1.0", tk.END).strip()
+
+        if input_text:
+            api_url = f"https://api-inference.huggingface.co/models/{selected_model}"
+            headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "application/json"}
+            payload = {"inputs": [input_text]}  # Adjust payload to list format
+
+            try:
+                response_data = self.query_huggingface_api(api_url, headers, payload)
+                formatted_response = json.dumps(response_data, indent=2)
+                self.output_text.config(state=tk.NORMAL)
+                self.output_text.delete("1.0", tk.END)
+                self.output_text.insert(tk.END, formatted_response)
+                self.output_text.config(state=tk.DISABLED)
+            except Exception as e:
+                error_message = f"Error: {str(e)}"
+                self.output_text.config(state=tk.NORMAL)
+                self.output_text.delete("1.0", tk.END)
+                self.output_text.insert(tk.END, error_message)
+                self.output_text.config(state=tk.DISABLED)
+        else:
+            self.output_text.config(state=tk.NORMAL)
+            self.output_text.delete("1.0", tk.END)
+            self.output_text.insert(tk.END, "Please enter input text.")
+            self.output_text.config(state=tk.DISABLED)
+
+    @staticmethod
+    def query_huggingface_api(api_url, headers, payload):
+        response = requests.post(api_url, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()
 
 
-
-# Run the app
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = HuggingFaceGUI(root)
+    root.mainloop()
